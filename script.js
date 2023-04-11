b1a971f3ddc2b436cc28c0f79d3fd4a652f496d5
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-11 12:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	if(num==0){
+		return 0;
+	}
+	let ans=0,a=1,b=0;
+	for(let i=0;i<num;i++){
+		ans = a+b;
+		a = b;
+		b = ans;
+	}
+	return b;
 }
 
 module.exports = fibonacci;

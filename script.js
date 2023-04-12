0be307b503a105175420c93d0a7cdd809fb51e14
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-12 11:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fibonacci(num) {
 // your code here
-	if(num==0){
+	if(num==0 || num==1){
 		return 0;
 	}
-	let ans=0,a=1,b=0;
-	for(let i=0;i<num;i++){
+	let ans=0,a=0,b=1;
+	for(let i=1;i<num-1;i++){
 		ans = a+b;
 		a = b;
 		b = ans;
